a811454d74bf5730d05abfd63425976c202e7025
Marin Marinov <108364779+Marin327@users.noreply.github.com>
2024-04-19 21:09:47 +0300

    LanguageContext.js

--- Workshop-Fitness-Website-BG-EN-main/src/LanguageContext/LanguageContext.js
+++ Workshop-Fitness-Website-BG-EN-main/src/LanguageContext/LanguageContext.js
@@ -1,18 +1,18 @@
 import React, { createContext, useContext, useState } from 'react';
 
-// Създаваме контекст за езика
+
 const LanguageContext = createContext();
 
-// Компонент за обвиване на приложението с контекста на езика
-export const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState('bg'); // По подразбиране езикът е български
 
-  // Функция за превключване на езика
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('bg'); 
+
+
   const toggleLanguage = () => {
     setLanguage(language === 'bg' ? 'en' : 'bg');
   };
 
-  // Предаваме текущия език, функцията за промяна на езика и самият език на децата
+ 
   return (
     <LanguageContext.Provider value={{ language, setLanguage, toggleLanguage }}>
       {children}
@@ -20,7 +20,7 @@ export const LanguageProvider = ({ children }) => {
   );
 };
 
-// Хук за използване на контекста на езика във функционални компоненти
+
 export const useLanguage = () => useContext(LanguageContext);
 
 export default LanguageContext;
